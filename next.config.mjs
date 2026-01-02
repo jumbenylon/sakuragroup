7f26b27982e6dda0f53922120b5b4e42ece1935f
Jumbe Seleman <jumbenylon@gmail.com>
2026-01-02 10:08:02 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
         port: '',
