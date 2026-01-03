c378afd70ddf8fee08b682771360d8bfb32b73c8
Jumbe Seleman <jumbenylon@gmail.com>
2026-01-03 17:39:06 +0300

    Update GoogleAnalytics.tsx

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,14 +3,18 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics({ gaId }: { gaId: string }) {
+  // Guard clause: If no ID is provided, do not inject script
   if (!gaId) return null;
 
   return (
     <>
+      {/* 1. Load the GTAG library asynchronously */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
         strategy="afterInteractive"
       />
+      
+      {/* 2. Initialize the Data Layer */}
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -19,6 +23,7 @@ export default function GoogleAnalytics({ gaId }: { gaId: string }) {
 
           gtag('config', '${gaId}', {
             page_path: window.location.pathname,
+            send_page_view: true
           });
         `}
       </Script>
