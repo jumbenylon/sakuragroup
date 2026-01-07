378a62de2627769e9e564758de599143915ec5a7
Jumbe Seleman <jumbenylon@gmail.com>
2026-01-07 14:13:37 +0300

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
-import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
